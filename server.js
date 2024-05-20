fc89a2a9ccdc63aa270b2e30d87479ed9f49c93d
Sean Yuan <ysean54@yahoo.com>
2024-05-20 13:14:49 -0400

    added memory in vercel.json

--- server.js
+++ server.js
@@ -13,27 +13,35 @@ mongoose.connect(process.env.MONGOURL)
 .then(() => console.log("connection successful"))
 .catch(() => console.log("error connecting to mongodb"))
 
-app.post('/login', (req,res) => {
-    const {email, password} = req.body;
-    UserModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            if(user.password === password){
-                res.json("success")
-            }else{
-                res.json("incorrect password")
-            }
-        }else{
-            res.json('user does not exist')
+app.post('/login', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await UserModel.findOne({ email });
+  
+      if (user) {
+        if (user.password === password) {
+          res.json("success");
+        } else {
+          res.json("incorrect password");
         }
-    })
-})
+      } else {
+        res.json('user does not exist');
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
-app.post("/signup", (req,res) => {
-    UserModel.create(req.body)
-    .then(users => res.json(users))
-    .catch(err => console.log(err))
-})
+app.post("/signup", async (req, res) => {
+    try {
+      const user = await UserModel.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 app.listen(port, () => {
     console.log(`successfully connected to port ${port}`)
